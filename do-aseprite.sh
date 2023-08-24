da61bbcbbee97b0818c182b50080042677ad83f5
RickGalasio <ricleitepessoal@gmail.com>
2023-08-24 17:30:48 -0300

    Updates work on ubuntu 22.04

--- do-aseprite.sh
+++ do-aseprite.sh
@@ -2,15 +2,33 @@
 # Rick Galasio (Ricardo Leite Gonçalves)
 # 25/01/2020
 
+
+set -ev
+
 AQUI=$(pwd)
 
 #Download requisitos
 sudo apt -y install \
+  expat \
+  libexpat1-dev \
+  clang \
+  libc++-dev \
+  libc++abi-dev \
+  build-essential \
+  checkinstall \
+  zlib1g-dev \
   dialog \
-  python \
+  python3 \
   g++ \
   cmake \
   ninja-build \
+  libfreetype-dev \
+  libharfbuzz-dev \
+  libacl1-dev \
+  xattr \
+  libbsd-dev \
+  libwebp-dev \
+  libssl-dev \
   libx11-dev \
   libxcursor-dev \
   libxi-dev \
@@ -22,22 +40,36 @@ sudo apt -y install \
   liblua5.3-dev \
   libluajit-5.1-dev
 
+#wget https://github.com/Kitware/CMake/releases/download/<version>/cmake-<version>.tar.gz
+
 VER_ASE=$(dialog --no-ok --no-cancel --title "Aseprite Branches" --menu "Aseprite Ver."  0 0 0 $(git ls-remote --tags https://github.com/aseprite/aseprite | sed -e 's|.*refs/tags/||g' | awk '{print $1 " " $1}') --stdout)
 VER_SKIA=$(dialog --no-ok --no-cancel --title "SKIA Branches" --menu "SKIA Ver."  0 0 0 $(git ls-remote --tags https://github.com/aseprite/skia | sed -e 's|.*refs/tags/||g' | awk '{print $1 " " $1}') --stdout)
 
 #Downloads dos fontes
-git clone -b $VER_ASE https://github.com/aseprite/aseprite.git --recurse-submodules aseprite-$VER_ASE/
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
-git clone -b $VER_SKIA https://github.com/aseprite/skia.git
+if [ ! -d aseprite-$VER_ASE ]; then
+   git clone -b $VER_ASE https://github.com/aseprite/aseprite.git --recurse-submodules aseprite-$VER_ASE/
+fi
+
+if [ ! -d depot_tools ]; then
+   git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+
+if [ ! -d skia ]; then
+   git clone -b $VER_SKIA https://github.com/aseprite/skia.git
+fi
 
 #Adiciona o caminho das ferramentas ao PATH
 export PATH="${PWD}/depot_tools:${PATH}"
 
 #Compilar skia
 cd skia
+if [ ! -f /usr/bin/python ]; then
+       ln -s /usr/bin/python3 /usr/bin/python
+fi       
 python tools/git-sync-deps
 gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true skia_use_system_freetype2=false skia_use_system_harfbuzz=false"
+ln -fs /usr/bin/ninja build.ninja
 ninja -C out/Release-x64 skia modules
 cd ..
 
